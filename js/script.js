0738b3ca0659723e404f19c545ec0c5f8ba83f18
BrendoAires <brendoc.aires@gmail.com>
2021-12-30 19:30:03 -0300

    fix bugs

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 const linkMenu = document.querySelector("#menu");
 linkMenu.addEventListener('click', ()=>{
 	const element = document.getElementById("menu-drop");
-    if (element.style.display === 'block') {
-        element.style.display = 'none';
+    if (element.style.display === 'none') {
+        element.style.display = 'block';
     } else {
-        element.style.display = 'block'; 
+        element.style.display = 'none'; 
     }
 })
 
