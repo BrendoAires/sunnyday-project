4fde1e7c6814b19d52d669123a5dd482810e7cdb
BrendoAires <brendoc.aires@gmail.com>
2022-01-06 19:32:19 -0300

    fix bugs

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ linkMenu.addEventListener('click', ()=>{
 	const element = document.getElementById("menu-drop");
     if (element.style.display === 'none') {
         element.style.display = 'block';
+        element.classList.add('.abrir');
+
     } else {
         element.style.display = 'none'; 
     }
